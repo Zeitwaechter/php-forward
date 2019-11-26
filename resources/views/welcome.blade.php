730a58b6cb64acb7c4b7e068f8f1c95862fadeeb
Zeitwaechter <zeitwaechter@nullnan.de>
2019-11-26 08:56:36 +0100

    Title.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>
+        {{--{{ __('strings.welcome.forward_title') }}--}}
+        Wir leiten dich gerne weiter
+    </title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
